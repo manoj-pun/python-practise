1f78d8577a411dcde3396281ff1dc40021035b10
manoj-pun <manojpun840@gmail.com>
2026-02-23 16:17:18 +0545

    Day 1 question 1

---
+++ Day1/question1.py
@@ -0,0 +1,7 @@
+#Write a program that takes a user's name, age, and city as input and prints a formatted introduction sentence.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print(f"My name is {name}. I am {age} year's old. I am currently living in {city}.")
