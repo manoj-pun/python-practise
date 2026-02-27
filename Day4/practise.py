d65e3ef1059dc6654b150e4480bb378baacdef4f
manoj-pun <manojpun840@gmail.com>
2026-02-27 09:26:06 +0545

    Day4 question1

---
+++ Day4/practise.py
@@ -0,0 +1,5 @@
+text = "hello"
+left = 0
+right = len(text) - 1
+print(text[left])
+print(text[right])
--- Day4/question1.py
+++ Day4/question1.py
@@ -1,1 +1,58 @@
+# palindrome for number
+# def is_palindrome(num):
+#     temp = num
+#     reverse = 0
+
+#     while temp > 0:
+#         digit = temp % 10
+#         reverse = reverse * 10 + digit
+#         temp //= 10
+
+#     return num == reverse
+
+
+# num = int(input("Enter the number: "))
+
+# if is_palindrome(num):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
 #Write a function is_palindrome(s) that returns True if a string is a palindrome without using slicing.
+def is_palindrome(text):
+    left = 0
+    right = len(text) - 1
+
+    while left < right:
+        if text[left] != text[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+text = input("Enter text: ")
+
+if is_palindrome(text):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+#with slicing
+def is_palindrome(text):
+    return text == text[::-1]
+
+text = input("Enter text: ")
+
+if is_palindrome(text):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+
